235faebd05a654d807ba3f15c1f8018e12dfbb58
avllgrn <avillagran@ipn.mx>
2025-09-25 07:53:15 -0600

    Cuenta nodos primos en una LSE con aleatorios

--- programa.py
+++ programa.py
@@ -2,11 +2,21 @@ from random import randrange
 from os import system
 from ClaseLSE import LSE
 
-def cuentaImparesEn(L):
+def esPrimo(n):
+    if n==0 or n==1:
+        return False
+
+    for i in range(2,n):
+        if n%i==0:
+            return False
+
+    return True
+
+def cuentaPrimosEn(L):
     pares = 0
     aux = L.primero
     while aux!=None:
-        if aux.dato%2 != 0:
+        if esPrimo(aux.dato) != 0:
             pares += 1
         aux=aux.siguiente
     return pares
@@ -19,8 +29,8 @@ if __name__ == '__main__':
     n = randrange(10)
 
     for i in range(n):
-        L.insertaAlUltimo(randrange(100))
+        L.insertaAlUltimo(randrange(10))
 
     print(f'L {L}')
 
-    print(f'Hay {cuentaImparesEn(L)} impar(es).')
+    print(f'Hay {cuentaPrimosEn(L)} primo(s).')
