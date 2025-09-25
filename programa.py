442ae4a52dd07e4f1accedd5dce3b816a074f901
avllgrn <avillagran@ipn.mx>
2025-09-25 09:02:03 -0600

    Genera LSE invertida, a partir de otra

--- programa.py
+++ programa.py
@@ -2,37 +2,27 @@ from random import randrange
 from os import system
 from ClaseLSE import LSE
 
-def concatena(L1, L2):
-    L3 = LSE()
+def generaInvertida(L):
+    I = LSE()
+    aux=L.primero
+    while aux!=None:
+        I.insertaAlPrimero(aux.dato)
+        aux=aux.siguiente
 
-    aux=L1.primero
-    while aux!=None:
-        L3.insertaAlUltimo(aux.dato)
-        aux=aux.siguiente
-    
-    aux=L2.primero
-    while aux!=None:
-        L3.insertaAlUltimo(aux.dato)
-        aux=aux.siguiente
-    
-    return L3
+    return I
 
 if __name__ == '__main__':
     system('cls')
 
     L1 = LSE()
-    L2 = LSE()
 
-    n1 = randrange(5)
-    for i in range(n1):
+    n = randrange(10)
+
+    for i in range(n):
         L1.insertaAlUltimo(randrange(100))
 
-    n2 = randrange(5)
-    for i in range(n2):
-        L2.insertaAlUltimo(randrange(100))
-
-    L3 = concatena(L1, L2)
     print(f'L1 {L1}')
+    print('\n\n\n')
+    L2 = generaInvertida(L1)
     print(f'L2 {L2}')
-    print(f'L3 {L3}')
     print('\n\n\n')
