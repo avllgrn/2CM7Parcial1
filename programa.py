61e2abf36aa431278b334310ed620e6bbe1a4343
avllgrn <avillagran@ipn.mx>
2025-09-25 08:56:28 -0600

    Concatena dos LSE

--- programa.py
+++ programa.py
@@ -2,36 +2,37 @@ from random import randrange
 from os import system
 from ClaseLSE import LSE
 
-def muestraInvertida(L):
-    aux1=L.primero
-    aux2=L.ultimo
+def concatena(L1, L2):
+    L3 = LSE()
+
+    aux=L1.primero
+    while aux!=None:
+        L3.insertaAlUltimo(aux.dato)
+        aux=aux.siguiente
     
-    while aux1!=aux2:
-        print(f'| {aux2.dato} |', end='')
-        if(aux2!=L.primero):
-            print(' <- ', end='')
-
-        while aux1.siguiente!=aux2:
-            aux1=aux1.siguiente
-
-        aux2=aux1
-        aux1=L.primero
-
-    if not L.estaVacia() and aux1==aux2:
-        print(f'| {aux2.dato} |', end='')
-
+    aux=L2.primero
+    while aux!=None:
+        L3.insertaAlUltimo(aux.dato)
+        aux=aux.siguiente
+    
+    return L3
 
 if __name__ == '__main__':
     system('cls')
 
-    L = LSE()
+    L1 = LSE()
+    L2 = LSE()
 
-    n = randrange(10)
+    n1 = randrange(5)
+    for i in range(n1):
+        L1.insertaAlUltimo(randrange(100))
 
-    for i in range(n):
-        L.insertaAlUltimo(randrange(100))
+    n2 = randrange(5)
+    for i in range(n2):
+        L2.insertaAlUltimo(randrange(100))
 
-    print(f'L {L}')
-    print('\n\n\n')
-    muestraInvertida(L)
+    L3 = concatena(L1, L2)
+    print(f'L1 {L1}')
+    print(f'L2 {L2}')
+    print(f'L3 {L3}')
     print('\n\n\n')
